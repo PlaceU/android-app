3984dc489b543d3fc92b438ea9f6ceac045d67cf
jbrigido <jbrigido_14@hotmail.com>
2019-12-05 16:38:29 +0000

    Initial Commit

---
+++ app/src/main/java/amsi/dei/estg/ipleiria/placeu/LoginActivity.java
@@ -0,0 +1,17 @@
+package amsi.dei.estg.ipleiria.placeu;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
